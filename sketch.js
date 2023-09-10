fc2c2269fa8c5a28602f080b8a6fa90fbf56a5da
profpco <133040950+profpco@users.noreply.github.com>
2023-09-10 18:05:42 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 600);
+  background("pink");
+}
+
+function draw() {
+  
+  stroke("white");
+  fill("black");
+  
+  // console.log(mouseIsPressed);
+  
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 10, 10);
+  }
+  
+}
